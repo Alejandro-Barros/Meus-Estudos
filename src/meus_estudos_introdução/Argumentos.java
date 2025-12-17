bd48c645e77b8d3a930471da6b4bc4f1cc40d38e
Alejandro-Barros <alejandro.freire00@gmail.com>
2025-12-16 22:34:42 -0300

    Primeiro Commit!

---
+++ src/meus_estudos_introdução/Argumentos.java
@@ -0,0 +1,11 @@
+package meus_estudos_introdução;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Argumentos {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Você digitou " + args[0]);
+    }
+}
---
+++ src/meus_estudos_scanner/LeituraDadosTeclado.java
@@ -0,0 +1,25 @@
+package meus_estudos_scanner;
+
+import java.util.Scanner;
+
+public class LeituraDadosTeclado {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite seu nome completo: ");
+        String nomeCompleto = scan.nextLine();
+        System.out.println("Seu nome completo é: " + nomeCompleto);
+
+        System.out.println("Digite sua idade: ");
+        int idadeUsuario = scan.nextInt();
+        System.out.println("Sua idade é: " + idadeUsuario);
+
+        System.out.println("Digite sua altura: ");
+        double alturaUsuario = scan.nextDouble();
+        System.out.println("Sua idade é: " + alturaUsuario);
+
+        System.out.println("Seu nome completo é " + nomeCompleto + ", sua idade é " + idadeUsuario);
+
+    }
+}
