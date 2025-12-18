f6926b4136e932c5985b294ca75d8213c1be2fd6
Alejandro-Barros <alejandro.freire00@gmail.com>
2025-12-17 23:30:23 -0300

    Estudo de operadores

--- src/meus_estudos_operadores/OperadoresAritimeticos.java
+++ src/meus_estudos_operadores/OperadoresAritimeticos.java
@@ -1,4 +1,28 @@
 package meus_estudos_operadores;
 
 public class OperadoresAritimeticos {
+    public static void main(String[] args) {
+
+        int resultado = 1 + 2;
+        System.out.println(resultado);
+
+        resultado = resultado - 1;
+        System.out.println(resultado);
+
+        resultado = resultado * 5;
+        System.out.println(resultado);
+
+        resultado = resultado / 2;
+        System.out.println(resultado);
+
+
+        String primeiraFrase = "Essa é a primeira frase";
+        String segundaFrase = " e essa, é a segunda frase";
+        String frasesConcaternadas = primeiraFrase + segundaFrase;
+        System.out.println(frasesConcaternadas);
+
+        System.out.println(resultado++);
+
+
+    }
 }
