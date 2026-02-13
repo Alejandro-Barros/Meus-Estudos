368e76defe126163b6229e13e8787bdb9816755a
Alejandro-Barros <alejandro.freire00@gmail.com>
2026-02-13 12:38:00 -0300

    Continuação dos estudos sobre orientação à objetos

--- src/meus_estudos_orientacaoaobjetos/Lampada.java
+++ src/meus_estudos_orientacaoaobjetos/Lampada.java
@@ -10,4 +10,26 @@ public class Lampada {
     int garantiaMeses;
     String [] tipos;
     boolean abajur;
+    boolean ligada;
+
+    void ligarLampada(){
+        ligada = true;
+    }
+    void desligarLampada(){
+        ligada = false;
+    }
+    void mostrarEstado(){
+        if (ligada){
+            System.out.println("A lâmpada está ligada");
+        } else {
+            System.out.println("A lâmpada está desligada");
+        }
+    }
+    void mudarEstado(){
+        if (ligada){
+            desligarLampada();
+        } else {
+            ligarLampada();
+        }
+    }
 }
